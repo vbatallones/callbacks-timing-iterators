bcdd57764385bf6b1db7f68de3d9a997bdb58fde
Levin Batallones <levinbatallones@Batallones.local>
2020-07-16 15:07:59 -0700

    Add greeting and other callback function for another greeting

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-function greeting (name) {
+function greeting (name, callback) {
     let sentence = 'Hello, my name is '+ name;
 
+   let otherSentence = callback();
+
+   return sentence + otherSentence
+}
+
+function anotherGreeting() {
+    let sentence = ' I forgot my last name';
+    
     return sentence;
 }
+
+let myName = 'Levin';
+console.log(greeting(myName, anotherGreeting));
