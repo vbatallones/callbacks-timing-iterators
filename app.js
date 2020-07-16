d1c2cd72bdd84f98a83ce64a41e60c85d306d970
Levin Batallones <levinbatallones@Batallones.local>
2020-07-16 15:32:34 -0700

    Add a calculator

--- app.js
+++ app.js
@@ -1,16 +1,56 @@
-function greeting (name, callback) {
-    let sentence = 'Hello, my name is '+ name;
+// function greeting (name, callback) {
+//     let sentence = 'Hello, my name is '+ name;
 
-   let otherSentence = callback();
+//    let otherSentence = callback();
 
-   return sentence + otherSentence
-}
+//    return sentence + otherSentence
+// }
 
-function anotherGreeting() {
-    let sentence = ' I forgot my last name';
+// function anotherGreeting() {
+//     let sentence = ' I forgot my last name';
     
-    return sentence;
+//     return sentence;
+// }
+
+// let myName = 'Levin';
+// console.log(greeting(myName, anotherGreeting));
+
+// function printArray(array, callback) {
+//     for (let i = 0; i < array.length; i++) {
+//         let numbers = array[i];
+
+//         console.log(numbers);
+//     }
+//     callback(array);
+// }
+
+// function printArrayAgain(array) {
+//     for (let i = 0; i < array.length; i++) {
+//         let number = array[i];
+
+//         console.log(number);
+//     }
+// }
+
+// const numberArray = [1,2,3,4,5,6,7,8,9];
+
+// printArray(numberArray, printArrayAgain);
+
+function calculator (num1, num2, callbackOne, callbackTwo) {
+    let result = callbackOne(num1, num2) + callbackTwo(num1, num2);
+
+    return result;
 }
 
-let myName = 'Levin';
-console.log(greeting(myName, anotherGreeting));
+function addNumbers(num1, num2) {
+  
+    let result = num1 + num2; 
+    return result;
+}
+
+function subtractNumbers(num1, num2) {
+    let result = num1 - num2;
+    return result;
+}
+
+console.log(calculator(5,77, addNumbers, subtractNumbers));
